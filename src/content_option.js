c6bc68b815dd1739ffb0031ce791bd8e7960d972
Ankush804 <ankush.pal@cpcmail.in>
2025-01-13 22:23:38 +0530

    work experience added

--- src/content_option.js
+++ src/content_option.js
@@ -1,166 +1,187 @@
 const logotext = "ANKUSH";
 const meta = {
-    title: "Ankush Pal",
-    description: "I’m Ankush Pal Full stack developer,currently working in Bangalore.",
+  title: "Ankush Pal",
+  description:
+    "I’m Ankush Pal Full stack developer,currently working in Bangalore.",
 };
 
 const introdata = {
-    title: "I’m Ankush Pal",
-    animated: {
-        first: "I love coding",
-        second: "I code cool websites",
-        third: "I develop mobile apps",
-    },
-    description: "Results-oriented Frontend Developer with 2+ years of experience in building dynamic, responsive web applications. Proficient in React, Redux, JavaScript, TypeScript, and RESTful APIs.",
-    your_img_url: "./profile-pic.jpg",
+  title: "I’m Ankush Pal",
+  animated: {
+    first: "I love coding",
+    second: "I code cool websites",
+    third: "I develop mobile apps",
+  },
+  description:
+    "Results-oriented Frontend Developer with 2+ years of experience in building dynamic, responsive web applications. Proficient in React, Redux, JavaScript, TypeScript, and RESTful APIs.",
+  your_img_url: "./profile-pic.jpg",
 };
 
 const dataabout = {
-    title: "Abit about my self",
-    aboutme: "I have graduated in 2020 with majors computer science engineering from Bharat Institute of Technology. From past 3 years I have been exploring the frontend development field majorly focused on React.js and Angular framework. With a passion for user-centric design, I've contributed to dynamic content loading and real-time data updates, ensuring responsive UI/UX that aligns with our clients needs.",
+  title: "Abit about my self",
+  aboutme:
+    "I have graduated in 2020 with majors computer science engineering from Bharat Institute of Technology. From past 3 years I have been exploring the frontend development field majorly focused on React.js and Angular framework. With a passion for user-centric design, I've contributed to dynamic content loading and real-time data updates, ensuring responsive UI/UX that aligns with our clients needs.",
 };
-const worktimeline = [{
-        jobtitle: "Product Engineer",
-        where: "Edgeverve",
-        date: "Feb 2022 - Sep 2023",
-    },
-    {
-        jobtitle: "Software Developer - Javascript",
-        where: "IT Hours Solutions",
-        date: "Oct 2020 - Feb 2022",
-    },
-    {
-        jobtitle: "Full Stack Web Development Trainee ",
-        where: "Newton School",
-        date: "Jul 2021 - Jan 2022",
-    },
+const worktimeline = [
+  {
+    jobtitle: "SDE-II",
+    where: "Chaitanya Projects Consultancy Private Limited",
+    date: "Dec 2024 - Present",
+  },
+  {
+    jobtitle: "Product Engineer",
+    where: "Edgeverve",
+    date: "Feb 2022 - Sep 2023",
+  },
+  {
+    jobtitle: "Software Developer - Javascript",
+    where: "IT Hours Solutions",
+    date: "Oct 2020 - Feb 2022",
+  },
+  {
+    jobtitle: "Full Stack Web Development Trainee ",
+    where: "Newton School",
+    date: "Jul 2021 - Jan 2022",
+  },
 ];
 
-const skills = [{
-        name: "React",
-        value: 90,
-    },
-    {
-        name: "Redux/Redux-toolkit",
-        value: 75,
-    },
-    {
-        name: "Javascript",
-        value: 85,
-    },
-    {
-        name: "HTML/CSS",
-        value: 85,
-    },
-    {
-        name: "Node JS",
-        value: 70,
-    },
-    {
-        name: "Java",
-        value: 65,
-    },
-    {
-        name: "Databases - MySql/MongoDB/PostgreSql",
-        value: 75,
-    },
-    {
-        name: "Data structures and algorithms",
-        value: 80,
-    },
+const skills = [
+  {
+    name: "React",
+    value: 90,
+  },
+  {
+    name: "Redux/Redux-toolkit",
+    value: 75,
+  },
+  {
+    name: "Javascript",
+    value: 85,
+  },
+  {
+    name: "HTML/CSS",
+    value: 85,
+  },
+  {
+    name: "Node JS",
+    value: 70,
+  },
+  {
+    name: "Java",
+    value: 65,
+  },
+  {
+    name: "Databases - MySql/MongoDB/PostgreSql",
+    value: 75,
+  },
+  {
+    name: "Data structures and algorithms",
+    value: 80,
+  },
 ];
 
-const services = [{
-        title: "Website Development",
-        description: "I bring 2+ years of experience in building modern, responsive, and dynamic web applications using React. I specialize in developing clean, efficient, and scalable code to create websites that are both visually appealing and highly functional. From building reusable components to optimizing performance, I ensure that every project meets industry standards and delivers a seamless experience across devices.",
-    },
-    {
-        title: "UI & UX Design",
-        description: "I specialize in crafting user-friendly and visually engaging interfaces using React. With a strong focus on both aesthetics and usability, I ensure seamless user experiences by designing intuitive layouts, responsive components, and interactive features that align with modern design principles. My approach combines creativity with functionality, delivering interfaces that not only look great but also provide an efficient and enjoyable user journey.",
-    },
-    {
-        title: "Data Structures and Algorithms",
-        description: "I specialize in designing efficient algorithms and leveraging data structures to solve complex problems. My expertise includes optimizing application performance, implementing scalable solutions, and enhancing code efficiency. Whether it’s improving computational logic or tackling algorithmic challenges, I bring a strong analytical approach to create robust and maintainable systems.",
-    },
+const services = [
+  {
+    title: "Website Development",
+    description:
+      "I bring 2+ years of experience in building modern, responsive, and dynamic web applications using React. I specialize in developing clean, efficient, and scalable code to create websites that are both visually appealing and highly functional. From building reusable components to optimizing performance, I ensure that every project meets industry standards and delivers a seamless experience across devices.",
+  },
+  {
+    title: "UI & UX Design",
+    description:
+      "I specialize in crafting user-friendly and visually engaging interfaces using React. With a strong focus on both aesthetics and usability, I ensure seamless user experiences by designing intuitive layouts, responsive components, and interactive features that align with modern design principles. My approach combines creativity with functionality, delivering interfaces that not only look great but also provide an efficient and enjoyable user journey.",
+  },
+  {
+    title: "Data Structures and Algorithms",
+    description:
+      "I specialize in designing efficient algorithms and leveraging data structures to solve complex problems. My expertise includes optimizing application performance, implementing scalable solutions, and enhancing code efficiency. Whether it’s improving computational logic or tackling algorithmic challenges, I bring a strong analytical approach to create robust and maintainable systems.",
+  },
 ];
 
-const dataportfolio = [{
+const dataportfolio = [
+  {
     img: "images/task-management.jpg",
-    description: "Application designed to help individuals and teams to organize, track, and complete tasks efficiently.",
+    description:
+      "Application designed to help individuals and teams to organize, track, and complete tasks efficiently.",
     link: "https://task-management-app-89c04.web.app/",
+  },
+  {
+    img: "images/todo-list.jpg",
+    description:
+      "A simple application to manage and organize tasks with options to add, edit, and delete items.",
+    link: "https://ankushpal98.github.io/todo-app/",
+  },
+  {
+    img: "images/school-management-system.jpg",
+    description:
+      "A school management system application that manages students data efficiently.",
+    link: "https://school-management-system-beta-wine.vercel.app/",
+  },
+  {
+    img: "images/reactflix.jpg",
+    description:
+      "A fully functional Reactflix web application for searching movies.",
+    link: "https://ankushpal98.github.io/Reactflix-React-Movie-App/",
+  },
+  {
+    img: "images/simon-game.jpg",
+    description:
+      "Memory strengthening and cognition improving simon game application.",
+    link: "https://ankushpal98.github.io/Simon-Game/",
+  },
+  {
+    img: "images/drum-kit.jpg",
+    description: "Drum kit website for making tunes and entertaining users.",
+    link: "https://ankushpal98.github.io/Drum-Kit/",
+  },
 
-    },
-    {
-        img: "images/todo-list.jpg",
-        description: "A simple application to manage and organize tasks with options to add, edit, and delete items.",
-        link: "https://ankushpal98.github.io/todo-app/",
-    },
-    {
-        img: "images/school-management-system.jpg",
-        description: "A school management system application that manages students data efficiently.",
-        link: "https://school-management-system-beta-wine.vercel.app/",
-    },
-    {
-        img: "images/reactflix.jpg",
-        description: "A fully functional Reactflix web application for searching movies.",
-        link: "https://ankushpal98.github.io/Reactflix-React-Movie-App/",
-    },
-    {
-        img: "images/simon-game.jpg",
-        description: "Memory strengthening and cognition improving simon game application.",
-        link: "https://ankushpal98.github.io/Simon-Game/",
-    },
-    {
-        img: "images/drum-kit.jpg",
-        description: "Drum kit website for making tunes and entertaining users.",
-        link: "https://ankushpal98.github.io/Drum-Kit/",
-    },
-
-    {
-        img: "images/discover-page.jpg",
-        description: "Explore a wide range of topics, such as tech, sports, finance, and more..",
-        link: "https://discover-page-clone.firebaseapp.com/",
-
-    },
-    {
-        img: "images/meal-finder.jpg",
-        description: "A web application developed for searching meals with ingredients and recipie.",
-        link: "https://ankushpal98.github.io/Meal-Finder-App/",
-
-    },
-    {
-        img: "images/snake-game.jpg",
-        description: "A classic game where the player controls a growing snake to eat food.",
-        link: "https://ankushpal98.github.io/snake-game/",
-    },
+  {
+    img: "images/discover-page.jpg",
+    description:
+      "Explore a wide range of topics, such as tech, sports, finance, and more..",
+    link: "https://discover-page-clone.firebaseapp.com/",
+  },
+  {
+    img: "images/meal-finder.jpg",
+    description:
+      "A web application developed for searching meals with ingredients and recipie.",
+    link: "https://ankushpal98.github.io/Meal-Finder-App/",
+  },
+  {
+    img: "images/snake-game.jpg",
+    description:
+      "A classic game where the player controls a growing snake to eat food.",
+    link: "https://ankushpal98.github.io/snake-game/",
+  },
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "ankkpundeer123@gmail.com",
-    YOUR_FONE: "8006763804",
-    description: "If you want any help, please reach out to me by providing your details in the given form.",
-    // creat an emailjs.com account 
-    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-    YOUR_SERVICE_ID: "service_xnf102a",
-    YOUR_TEMPLATE_ID: "template_quva3bx",
-    YOUR_USER_ID: "p6yrtjLpHWndx5r3U",
+  YOUR_EMAIL: "ankkpundeer123@gmail.com",
+  YOUR_FONE: "8006763804",
+  description:
+    "If you want any help, please reach out to me by providing your details in the given form.",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_xnf102a",
+  YOUR_TEMPLATE_ID: "template_quva3bx",
+  YOUR_USER_ID: "p6yrtjLpHWndx5r3U",
 };
 
 const socialprofils = {
-    github: "https://github.com/AnkushPal98",
-    linkedin: "https://www.linkedin.com/in/ankush-pal-42ba85174/",
-    instagram: "https://www.instagram.com/ankushpundeer98/",
-    leetcode: "https://leetcode.com/u/ankush_98/",
+  github: "https://github.com/AnkushPal98",
+  linkedin: "https://www.linkedin.com/in/ankush-pal-42ba85174/",
+  instagram: "https://www.instagram.com/ankushpundeer98/",
+  leetcode: "https://leetcode.com/u/ankush_98/",
 };
 export {
-    meta,
-    dataabout,
-    dataportfolio,
-    worktimeline,
-    skills,
-    services,
-    introdata,
-    contactConfig,
-    socialprofils,
-    logotext,
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
 };
